a634be6a00b213587875f0844c212ec17d56f1ad
zinminoo298 <niznimoo26111997@gmail.com>
2023-05-09 15:40:21 +0700

    EDP & QC finished UI

---
+++ app/src/main/java/com/example/hinoedp/List.kt
@@ -0,0 +1,11 @@
+package com.example.hinoedp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class List : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
