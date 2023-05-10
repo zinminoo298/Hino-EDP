d8441d7129e36662f91783b2323a032d111202eb
zinminoo298 <niznimoo26111997@gmail.com>
2023-05-10 14:38:02 +0700

    Login & Setting finished

--- app/src/main/java/com/example/hinoedp/EDPSetting.kt
+++ app/src/main/java/com/example/hinoedp/EDPSetting.kt
@@ -2,10 +2,35 @@ package com.example.hinoedp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class EDPSetting : AppCompatActivity() {
+    companion object{
+        lateinit var textViewDate: TextView
+        lateinit var textViewPartNo: TextView
+        lateinit var textViewTotalEDP: TextView
+        lateinit var spinnerOrderNo: Spinner
+        lateinit var spinnerStatus: Spinner
+        lateinit var editTextViewSKB: TextView
+        lateinit var buttonList: Button
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edpsetting)
+
+        textViewDate = findViewById(R.id.textview_date)
+        textViewPartNo = findViewById(R.id.textView_partNo)
+        textViewTotalEDP = findViewById(R.id.textview_edp)
+        spinnerOrderNo = findViewById(R.id.spinner_orderNo)
+        spinnerStatus = findViewById(R.id.spinner_status)
+        editTextViewSKB = findViewById(R.id.editText_skb)
+        buttonList = findViewById(R.id.button_list)
+
+        textViewDate.setOnClickListener {
+
+        }
+
     }
 }
--- app/src/main/java/com/example/hinoedp/MainActivity.kt
+++ app/src/main/java/com/example/hinoedp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.hinoedp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewEDP = findViewById<TextView>(R.id.textview_edp)
+        val textViewQC = findViewById<TextView>(R.id.textview_qc)
+
+        textViewEDP.setOnClickListener {
+            startActivity(EDPSetting::class.java)
+        }
+
+        textViewQC.setOnClickListener {
+            startActivity(QualityControl::class.java)
+        }
+    }
+
+    private fun startActivity(cls:Class<*>){
+        val intent = Intent(this,cls)
+        startActivity(intent)
     }
 }
